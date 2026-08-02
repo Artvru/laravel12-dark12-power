4032a7d5b29d319d45061686527ac676832dc0a4
Teerarak <Teerarak.ji@vru.ac.th>
2026-08-02 19:53:46 +0700

    งานสร้าง Controller ที่ชื่อว่า “CategoryController”

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -116,3 +117,32 @@ Route::get('/active/blog', function () {
 Route::get('/active/contact', function () {
     return view('active/contact');
 })->name('contact');
+
+//งานใหม่จากตรงนี้ route มันจะแปลกๆ หน่อย นั่นแหละงานใหม่
+Route::get('/test',function(){
+    return view('test');
+})->name('test');
+
+
+Route::get('/coronavirus',function(){
+    $reports = [
+        (object) ["country"=>"China" , "date"=>"2020-04-19" , "total"=>"2765", "active"=>"790"  , "death"=>"47", "recovered"=>"1928"],
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-18" , "total"=>"2733", "active"=>"899"  , "death"=>"47", "recovered"=>"1787"],
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-17" , "total"=>"2700", "active"=>"964"  , "death"=>"47", "recovered"=>"1689"],
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-16" , "total"=>"2672", "active"=>"1033" , "death"=>"46", "recovered"=>"1593"],
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-15" , "total"=>"2643", "active"=>"1103" , "death"=>"43", "recovered"=>"1497"],
+    ];
+    return view("coronavirus", compact("reports") );
+})->name('coronavirus');
+
+//ตรงนี้มันเป็นอีก route กันงง
+Route::get('/active/teacher', function () {
+    $teachers = json_decode(file_get_contents('https://raw.githubusercontent.com/arc6828/laravel8/main/public/json/teachers.json'));
+    return view("active.teacher", compact("teachers"));
+})->name('active.teacher');
+
+
+Route::get('/category/sport', [CategoryController::class, "sport"]);
+Route::get('/category/politic', [CategoryController::class, "politic"]);
+Route::get('/category/entertain', [CategoryController::class, "entertain"]);
+Route::get('/category/auto', [CategoryController::class, "auto"]);
